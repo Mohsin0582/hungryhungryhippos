fb9ed195fe82014fd8175db19b16f2a694e19de3
Mohsin0582 <m.mohsin.shahzad0582@gmail.com>
2022-09-01 23:55:07 +0500

    buid-fixed fonts

--- pages/index.js
+++ pages/index.js
@@ -2,39 +2,43 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Home() {
+const data = `Hungry hungry hippos will conduct a 
+            <br/>
+            fair launch in which 80&#65285; of pre raised 
+            <br/>
+            funds will be blocked in a secure 
+            <br/>
+            eth liquidity pool
+            <br/>
+            lock it with us &#1470; target 50&#65285; APR`;
+
   return (
     <div className="w-full lg:w-[1240px] mx-auto">
       <div className='h-[calc(100vh-184px)]'>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 h-full">
           <div className="col-span-0 sm:col-span-1 md:col-span-2 lg:col-span-3 flex flex-col justify-center items-center">
-            <h2 className='uppercase font-outline flex justify-center items-center text-center text-xl'>
-              Hungry hungry hippos will conduct a 
-              <br/>
-              fair launch in which 80% of pre raised 
-              <br/>
-              funds will be blocked in a secure 
-              <br/>
-              eth liquidity pool
-              <br/>
-              lock it with us - target 50% APR
-            </h2>
+            <h2 className='uppercase font-outline flex justify-center items-center text-center text-xl' dangerouslySetInnerHTML={{__html: data}} />
             <div className='uppercase flex items-center mt-8'>
               <Link href={``}>
                 <a className='border-4 border-black px-5 py-2 text-center md:mr-4'>
-                  Link to presale button
+                  <span className='text-lg'>L</span><span className='text-sm mr-1'>ink to</span>
+                  <span className='text-lg'>P</span><span className='text-sm mr-1'>resale</span>
+                  <span className='text-lg'>B</span><span className='text-sm'>utton</span>
                   <br/> 
                   <span className='text-sm'>"Presale info"</span>
                 </a>
               </Link>
               <Link href={``}>
                 <a className='border-4 border-black px-5 py-2 text-center'>
-                  <span className=''>Link to telegram group</span>
+                  <span className='text-lg'>L</span><span className='text-sm mr-1'>ink to</span>
+                  <span className='text-lg'>T</span><span className='text-sm mr-1'>elegram</span>
+                  <span className='text-lg'>G</span><span className='text-sm'>roup</span>
                   <br/> 
                   <span className='text-sm'>"Click here for telegram"</span>
                 </a>
               </Link>
             </div>
-            <p className='font-outline text-black uppercase font-bold text-center mt-4'>Contract address</p>
+            <p className='font-fill uppercase text-center mt-4 tracking-wide text-sm'>Contract address</p>
             <p className='uppercase text-center mt-4 text-lg'>0x921B1A01D3B9FCAD71C2BBF95FDA751844CDC139</p>
           </div>
           <div className="col-span-0 sm:col-span-1 md:col-span-2 lg:col-span-2">
@@ -43,8 +47,19 @@ export default function Home() {
             </div>
           </div> 
         </div>
-        <div>
-          <p className='font-outline text-black uppercase text-4xl text-center py-8'>Full Site Coming Soon</p>
+        <div className='flex justify-center items-center'>
+          <span className='font-outline text-black text-center py-8 tracking-widest text-2xl first-letter:text-4xl mr-2'>
+            Full 
+          </span>
+          <span className='font-outline text-black text-center py-8 tracking-widest text-2xl first-letter:text-4xl mr-2'>
+            Site 
+          </span>
+          <span className='font-outline text-black text-center py-8 tracking-widest text-2xl first-letter:text-4xl mr-2'>
+            Coming 
+          </span>
+          <span className='font-outline text-black text-center py-8 tracking-widest text-2xl first-letter:text-4xl mr-2'>
+            Soon 
+          </span>
         </div>
       </div>
     </div>
